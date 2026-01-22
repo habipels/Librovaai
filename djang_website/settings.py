067974a615cfbdc9f2f5cf82654c520258274685
Habip ELİŞ <46339672+habipels@users.noreply.github.com>
2026-01-22 20:04:34 +0300

    #upload

--- djang_website/settings.py
+++ djang_website/settings.py
@@ -65,7 +65,7 @@ ROOT_URLCONF = 'djang_website.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -130,6 +130,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
--- users/forms.py
+++ users/forms.py
@@ -1,8 +1,6 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm, SetPasswordForm, PasswordResetForm
 from django.contrib.auth import get_user_model
-from captcha.fields import ReCaptchaField
-from captcha.widgets import ReCaptchaV2Checkbox
 
 
 class UserRegistrationForm(UserCreationForm):
@@ -31,8 +29,6 @@ class UserLoginForm(AuthenticationForm):
     password = forms.CharField(widget=forms.PasswordInput(
         attrs={'class': 'form-control', 'placeholder': 'Password'}))
 
-    captcha = ReCaptchaField(widget=ReCaptchaV2Checkbox())
-
 class UserUpdateForm(forms.ModelForm):
     email = forms.EmailField()
 
@@ -48,5 +44,3 @@ class SetPasswordForm(SetPasswordForm):
 class PasswordResetForm(PasswordResetForm):
     def __init__(self, *args, **kwargs):
         super(PasswordResetForm, self).__init__(*args, **kwargs)
-
-    captcha = ReCaptchaField(widget=ReCaptchaV2Checkbox())
